3cd2e2b79b7c90df2114aee2e2f1f7fc4e92c434
GreatRoyce <royceokoh@gmail.com>
2025-08-09 15:49:38 +0100

    second commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./database/dbConnection");
+const routes = require("./routes/routes");
+const path = require("path")
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "./public")));
+
+
+app.use("/register", routes);
+
+app.get("/", (req, res) => {
+  res.status(200).json({message: `This is working`});
+});
+
+app.get("/register", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public", "index.html"));
+});
+
+const PORT =  9000;
+connectDB();
+app.listen(PORT, () => {
+  console.log(`🚀 Server running at http://localhost:${PORT}`);
+});
